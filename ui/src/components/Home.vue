33573e7489876a48671ca03ca39636422b94d1b3
Kent Moreland <kentmoreland@gmail.com>
2019-12-21 15:18:24 -0600

    30D-20: Added Vuetify

---
+++ ui/src/components/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <section class="home-content">
+    <h1>30 Day Workouts</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+};
+</script>
---
+++ ui/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home-page">
+    <Home/>
+  </div>
+</template>
+
+<script>
+import Home from '@/components/Home.vue';
+
+export default {
+  components: {
+    Home,
+  },
+};
+</script>
