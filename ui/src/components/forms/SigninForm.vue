0a5ce7749faf6e6415b47cce7ee9ab711fe1bb7f
Kent Moreland <kentmoreland@gmail.com>
2019-12-22 15:38:39 -0600

    30D-23: Adds signin form and functionality.

---
+++ ui/src/components/forms/SigninForm.vue
@@ -0,0 +1,45 @@
+<template>
+  <Form class=form>
+    <h1 class="title">30-Day Login</h1>
+    <div class="input-container">
+      <v-text-field label="Email" v-model="email" />
+      <v-text-field label="Password" type="password" v-model="password" />
+    </div>
+    <v-btn depressed color="grey" @click="submit">
+      <v-icon>person</v-icon>
+      Sign in
+    </v-btn>
+  </Form>
+</template>
+<script>
+import userController from '@/data/userController';
+
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+    };
+  },
+  methods: {
+    async submit() {
+      // get the result of this call and handle errors or set user.
+      await userController.signin(this.email, this.password);
+    },
+  },
+};
+</script>
+<style lang="scss">
+  .form {
+    background-color: white;
+    margin: auto;
+    padding: 1em;
+    width: 30em;
+    .title {
+      text-align: center;
+    }
+  }
+  .input-container {
+    margin-bottom: 1em;
+  }
+</style>
